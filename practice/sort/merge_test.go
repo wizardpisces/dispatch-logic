0a1b23d0f5c505bd9e9b5fd287624981c3524e8a
wizardpisces <1275839779@qq.com>
2023-01-16 15:09:30 +0800

    refactor: merge test cases

--- practice/sort/merge_test.go
+++ practice/sort/merge_test.go
@@ -16,6 +16,25 @@ type mergeSortSuite struct {
 	suite.Suite
 }
 
+// type test2 struct {
+// 	name string
+// }
+// type test3 struct {
+// 	name string
+// 	age  int
+// }
+// type test1 struct {
+// 	test2
+// 	test3
+// }
+
+// func (t test2) getName() string {
+// 	return t.name
+// }
+// func (t test3) getName() string {
+// 	return t.name
+// }
+
 func (s mergeSortSuite) Test_MergeSort() {
 	cases := []testCase{
 		{
@@ -30,12 +49,12 @@ func (s mergeSortSuite) Test_MergeSort() {
 			s.Equal(c.out, MergeSort(c.in))
 		})
 	}
-	// unsorted := []int{10, 6, 2, 1, 5, 8, 3, 4, 7, 9}
-	// sorted := MergeSort(unsorted)
-
-	// assert.Equal(t, sorted, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
 }
 
 func TestMergeSort(t *testing.T) {
+	// test := new(test1)
+	// test.name = "name"
+	// test.age = 1
+
 	suite.Run(t, new(mergeSortSuite))
 }
